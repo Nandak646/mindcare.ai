cb436390c4362c4e7581e16ca789eda33b072935
Nandak646 <nandakishorkumarreddy011@gmail.com>
2026-05-16 11:25:09 +0530

    Refactor app.py for better readability and structure

--- app.py
+++ app.py
@@ -203,11 +203,4 @@ def history():
 # -----------------------------------
 
 if __name__ == "__main__":
-
-    port = int(os.environ.get("PORT", 5000))
-
-    app.run(
-        host="0.0.0.0",
-        port=port,
-        debug=True
-    )
+    app.run(debug=True)
